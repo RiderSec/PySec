17056009b155855be51956c2953cb644bf3da5c8
RiderSec <58704510+RiderSec@users.noreply.github.com>
2024-11-11 12:09:15 +0100

    v1.1 - Added terminal interaction
    Added terminal interaction for the program to not require any changes by the user. Added input validation.

--- PySec.py
+++ PySec.py
@@ -1,3 +1,7 @@
+# Author: Rafael Rider / RiderSec
+
+
+
 import random
 
 
@@ -6,13 +10,48 @@ uppercase = lowercase.upper()
 digits = "123456789"
 symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 all = ""
+upper, lower, nums, syms, = False, False, False, False
+choice1 = None
 
 
+print("Welcome to PySec!\n")
 
-upper, lower, nums, syms, = True, True, True, True
 
-# ^^ Change any of these values to false to change the character set of your passwords. ^^
+while choice1 != 0:
+    print("Choose which characters PySec will use: ")
+    print("1 - All")
+    print("2 - Uppercase")
+    print("3 - Lowercase")
+    print("4 - Numbers")
+    print("5 - Symbols")
+    print("0 - Continue\n")
 
+    choice1 = input("Input: ")
+
+    try:
+        choice1 = int(choice1)
+    
+        if 0 <= choice1 <= 5:
+            match choice1:
+                case 1:
+                    upper, lower, nums, syms = True, True, True, True
+                case 2:
+                    upper = True
+                case 3:
+                    lower = True
+                case 4:
+                    nums = True
+                case 5:
+                    syms = True
+                case 0:
+                    print("Continuing...\n")
+                    break
+        
+        else: 
+            print("The input is not between 0 to 5, try again.")
+
+    except ValueError:
+        print("Invalid input, enter an integer.") 
 
 
 if upper: 
@@ -24,15 +63,24 @@ if nums:
 if syms:
     all += symbols
 
+length = input("How long should the passwords be?: ")
 
-length = 20
-amount = 10
+try: 
+    length = int(length)
 
-# ^^ Change these to change the length and amount, as needed. ^^
+except ValueError:
+    print("Invalid input, enter an integer.")
+
+amount = input("How many passwords should the program generate?: ")
+
+try: 
+    amount = int(amount)
+
+except ValueError:
+    print("Invalid input, enter an integer.")
 
 
 for x in range(amount):
     password = "".join(random.sample(all,length))
     print(password)
     
-
